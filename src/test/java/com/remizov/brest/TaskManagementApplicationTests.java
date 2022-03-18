14ae523bf49c62badc5b270ca7f76caf26d628ae
Alex Remizov <rem.remizov@yandex.ru>
2022-03-18 15:25:06 -0400

    Add start template

---
+++ src/test/java/com/remizov/brest/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remizov.brest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
