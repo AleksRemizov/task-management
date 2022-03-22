e7614faa97b49704f027a13d73063c3dbf74ea54
Alex Remizov <rem.remizov@yandex.ru>
2022-03-22 00:05:01 -0400

    Add entity clases

---
+++ src/main/java/com/remizov/brest/entity/Element.java
@@ -0,0 +1,77 @@
+package com.remizov.brest.entity;
+
+import javax.persistence.*;
+/**
+ * Simple JavaBean domain object representing an element.
+ *
+ * @author Alex Remizov
+ */
+
+@Entity
+@Table(name = "element")
+public class Element {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private String value;
+
+    @ManyToOne
+    @JoinColumn(name = "task_task_id",nullable = false)
+    private Task task;
+
+    public Task getTask() {
+        return task;
+    }
+
+    public Element(){
+    }
+
+    public Element(String name, String description, String value) {
+        this.name = name;
+        this.description = description;
+        this.value = value;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Element setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Element setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Element setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public Element setValue(String value) {
+        this.value = value;
+        return this;
+    }
+
+}
---
+++ src/main/java/com/remizov/brest/entity/Task.java
@@ -0,0 +1,134 @@
+package com.remizov.brest.entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.*;
+
+/**
+ * Simple JavaBean domain object representing a task.
+ *
+ * @author Alex Remizov
+ */
+
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String description;
+
+    @Column(name = "start_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    private Integer status;
+
+    @OneToMany(mappedBy = "task",cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OrderBy("start_date")
+    private Set<Element> elements;
+
+    public Task(){
+    }
+
+    public Task(String name, String password, String description, LocalDate startDate, LocalDate endDate,
+                Integer status) {
+        this.name = name;
+        this.password = password;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Task setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Task setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Task setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Task setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public Task setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+        return this;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public Task setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+        return this;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public Task setStatus(Integer status) {
+        this.status = status;
+        return this;
+    }
+
+    public Set<Element> getElements() {
+        return elements;
+    }
+
+    public void addElement(Element element){
+        if(elements == null){
+            elements = new HashSet<>();
+            elements.add(element);
+        }
+    }
+
+    public Task setElements(Set<Element> elements) {
+        this.elements = elements;
+        return this;
+    }
+}
